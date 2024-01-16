cce91824e17eddf705cbe1faacd133533e347fc3
Your Name <manab>
2024-01-16 20:09:22 +0530

    changes in jsson file

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import page from "./components/login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    
   </React.StrictMode>
 );
